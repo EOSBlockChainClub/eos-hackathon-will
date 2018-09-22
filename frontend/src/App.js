bee10a0d696092b76486cdb3e0b1a264acb6e3c0
Eugene Luzgin <eluzgin@Eugenes-MacBook-Pro.local>
2018-09-23 00:24:38 +0100

    Update chainid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ const network = {
   protocol:'https',
   host:'nodes.get-scatter.com',
   port:443,
-  chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+  chainId:'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
 }
 class App extends Component {
 
